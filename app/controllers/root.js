4385fa91b49fa889ae843c21173abd947def3a47
dylanbathurst <dylanbathurst@gmail.com>
2012-01-14 14:10:28 -0800

    redirecting to /dylanbathurst

--- app/controllers/root.js
+++ app/controllers/root.js
@@ -3,5 +3,7 @@ var express = require('express');
 var app = module.exports = express.createServer();
 
 app.get('/', function (req, res, next) {
-  res.json({ 'Welcome': 'to ApiMe', 'What is this?': 'Have you ever wanted your own API that\'s all about you? Well here you are! Just put your Twitter, Facebook, Gravatar, or Foursquare username after a forward slash in the address bar to get started' });
+
+  res.redirect('/dylanbathurst', 302);
+  /* res.json({ 'Welcome': 'to ApiMe', 'What is this?': 'Have you ever wanted your own API that\'s all about you? Well here you are! Just put your Twitter, Facebook, Gravatar, or Foursquare username after a forward slash in the address bar to get started' }); */
 });
